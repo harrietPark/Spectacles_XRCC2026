368e6121f9cae11bed16f1712996c84162092ea4
Jiayao Yu <yjybxx2@yahoo.com>
2026-04-20 15:37:23 +0200

    Add placeholder note anchoring index finger tip visual. Test in Spectacles.

--- Assets/Scripts/NoteController.ts
+++ Assets/Scripts/NoteController.ts
@@ -3,16 +3,34 @@ import { SceneManager } from "./SceneManager";
 
 @component
 export class NoteController extends BaseScriptComponent {
+    @ui.group_start("Crop to Photo")
     @input private PictureController: PictureController;
+    @ui.group_end
+    @ui.separator
+    @ui.group_start("Visual Feedback")
+    @input private MidasTouchVisual: SceneObject;
+    @ui.group_end
 
     public activateCreationProcess() {
+        this.activateNoteAnchoringVisual();
         this.PictureController.enableCrop();
     }
 
     public deactivateCreationProcess() {
+        this.deactivateNoteAnchoringVisual();
         this.PictureController.disableCrop();
     }
     
-    private onAwake() {}
+    private onAwake() {
+        this.deactivateNoteAnchoringVisual();
+    }
+
+    private activateNoteAnchoringVisual() {
+        this.MidasTouchVisual.enabled = true;
+    }
+
+    private deactivateNoteAnchoringVisual() {
+        this.MidasTouchVisual.enabled = false;
+    }
 
 }
